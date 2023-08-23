0db9926de874a6e699f8c80b067c02e80394b5aa
ankita <ankitacoder3@gmail.com>
2023-08-23 20:52:17 +0530

    modified server.py

--- server.py
+++ server.py
@@ -1,21 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-@app.route("/calculator/greeting", methods=['GET'])
+@app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return "Hello, World!"
+    return "Hello, world!",200
 
-@app.route("/calculator/add", methods=['POST'])
-def add(a,b):
-    result = a + b 
-    return jsonify({"result": result})
+@app.route('/calculator/add', methods=['POST'])
+def add():
+    data = request.json
+    first = data.get('first')
+    second = data.get('second')
 
-@app.route("/calculator/subtract", methods=['POST'])
-def subtract(a,b):
-    result = a - b  
-    return jsonify({"result": result})
+    if first is None or second is None:
+        return jsonify({"error": "Missing 'first' or 'second' parameter"}), 400
+
+    result = first + second
+    return jsonify({"result": result}),200
+
+@app.route('/calculator/subtract', methods=['POST'])
+def subtract():
+    data = request.json
+    first = data.get('first')
+    second = data.get('second')
+
+    if first is None or second is None:
+        return jsonify({"error": "Missing 'first' or 'second' parameter"}), 400
+
+    result = first - second
+    return jsonify({"result": result}),200
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run()
