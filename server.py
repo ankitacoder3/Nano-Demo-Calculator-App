2f8086f46ad61aad2d1e237c1249de302705de9d
ankita <ankitacoder3@gmail.com>
2023-08-23 21:24:56 +0530

    modified server.py

--- server.py
+++ server.py
@@ -5,31 +5,29 @@ from dataclasses import dataclass
 class Result:
     result: int
 
+from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello, world!'
+    return 'Hello world!'
+    return ''
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    data = request.json
-    if 'first' not in data or 'second' not in data:
-        return jsonify({"error": "Both 'first' and 'second' parameters are required"}), 400
-        
-    result = data['first'] + data['second']
-    response = Result(result)
+    numbers = request.json
+    response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
+    return ''
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data = request.json
-    if 'first' not in data or 'second' not in data:
-        return jsonify({"error": "Both 'first' and 'second' parameters are required"}), 400
-        
-    result = data['first'] - data['second']
-    response = Result(result)
+    numbers = request.json
+    response = Result(numbers['first'] - numbers['second'])
     return jsonify(response)
+    return ''
 
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
+    app.run(port=8080,host='0.0.0.0')
